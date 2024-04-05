c2fa57f6431d05a886bfdaf100d61d54264ef708
Gokul Chandra <gokulpatakula@gmail.com>
2024-04-04 19:22:39 -0500

    portfolio

---
+++ src/data/constants.js
@@ -0,0 +1,253 @@
+export const Bio = {
+  name: "Gokul Chandra",
+  roles: [
+    "Full Stack Developer",
+    "Back end Developer",
+    "Programmer",
+  ],
+  description:
+    "I am a Computer Science Graduate student at UNT actively seeking a Software Developer full time and internship to leverage my previous software development experience. With a solid foundation in mathematics and logical reasoning, I am equipped to deliver innovative software solutions that add value to organizations.",
+  github: "https://github.com/Gokul4Git",
+  resume:
+    "https://drive.google.com/file/d/1oXLAi639TIRQmN-yDjQybH20rhQQl7ea/view?usp=sharing",
+  linkedin: "https://www.linkedin.com/in/gokulpatakula/",
+  insta: "https://www.instagram.com/gokul_patakula/",
+  facebook: "https://www.facebook.com/profile.php?id=100008239485715"
+};
+
+export const skills = [
+  {
+    title: "Frontend",
+    skills: [
+      {
+        name: "React Js",
+        image:
+          "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
+      },
+      {
+        name: "Redux",
+        image:
+          "https://d33wubrfki0l68.cloudfront.net/0834d0215db51e91525a25acf97433051f280f2f/c30f5/img/redux.svg",
+      },
+     
+      {
+        name: "HTML",
+        image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+      },
+      {
+        name: "CSS",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+      },
+      {
+        name: "JavaScript",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+      },
+      {
+        name: "Bootstrap",
+        image:
+          "https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo-shadow.png",
+      },
+     
+    ],
+  },
+  {
+    title: "Backend",
+    skills: [
+      {
+        name: "Spring Boot",
+        image: "https://user-images.githubusercontent.com/33158051/103925017-e7673b80-50e4-11eb-9379-ceb82e3f382c.png",
+      },
+
+      {
+        name: "Java",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/java/java-original.svg",
+      },
+      
+      {
+        name: "Python",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg",
+      },
+      {
+        name: "MySQL",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg",
+      },
+      
+      {
+        name: "MongoDB",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original-wordmark.svg",
+      },
+      
+    ],
+  },
+  
+  
+  {
+    title: "Others",
+    skills: [
+      {
+        name: "Git",
+        image:
+        "https://upload.wikimedia.org/wikipedia/commons/3/3f/Git_icon.svg",
+      },
+      {
+        name: "GitHub",
+        image:
+          "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+      },
+      {
+        name: "Docker",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/docker/docker-original-wordmark.svg",
+      },
+      
+      {
+        name: "VS Code",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
+      },
+      {
+        name: "Postman",
+        image:
+          "https://i.pinimg.com/564x/51/5b/19/515b190130613d716ecdd008158f4c50.jpg",
+      },
+     
+      
+    ],
+  },
+];
+
+export const experiences = [
+  {
+    id: 0,
+    img: "https://i.pinimg.com/564x/82/bb/e1/82bbe12869fcd71b3ab789a970c11c48.jpg",
+    role: "Software Web Assistant",
+    company: "University Of North Texas",
+    date: "Aug 2022- May 2024",
+    desc: "Providing holistic IT support to faculty as the Web Assistant for the College of Health and Public Service, UNT",
+    skills: [
+      "MySQL",
+      "ServiceNow",
+      "JavaScript",
+      "Troubleshoot",
+    
+    ],
+  },
+  {
+    id: 0,
+    img: "https://i.pinimg.com/564x/5a/3b/96/5a3b96b4e7581ff9fd12da8eb72fd127.jpg",
+    role: "Software Engineer",
+    company: "Tata Consultancy Services",
+    date: "Nov 2020 - July 2022",
+    desc: " Develop a RESTful API using Spring by identifying requirements and resolving code issues throughout the project life cycle ",
+    skills: [
+      "Spring Boot",
+      "Spring",
+      "RestAPI",
+      "Java",
+      "MySQL",
+    ],
+  },
+  {
+    id: 1,
+    img: "https://i.pinimg.com/564x/76/5c/ea/765ceae200549062fab3c689ca07d054.jpg",
+    role: "Web developer intern",
+    company: "NITdata",
+    date: "December 2019 - May 2020",
+    desc: "Contributed to front-end development of the project using JavaScript and ReactJS.",
+    skills: [
+      "ReactJS",
+      "Redux",
+      "Javascript",
+      "HTML",
+      "CSS",
+    ],
+  },
+]
+
+  
+  
+    
+  
+export const education = [
+  {
+    id: 0,
+    img: "https://i.pinimg.com/564x/82/bb/e1/82bbe12869fcd71b3ab789a970c11c48.jpg",
+    school: "University Of North Texas",
+    date: "Aug 2022 - May 2024",
+    grade: "3.9/4",
+    desc: "I am currently pursuing a Master's degree in Computer Science at University Of North Texas.",
+    degree: "Master of Science - Msc, Computer Science",
+  },
+  {
+    id: 1,
+    img: "https://i.pinimg.com/564x/01/3b/48/013b486e8313497427fcd22a299c85c0.jpg",
+    school: "Chaitanya Bharathi Institute of Technology",
+    date: "August 2016 - Nov 2020",
+    grade: "3.6/4",
+    desc: "I completed my bachelor of engineering in CBIT, Hyderabad.",
+    degree: "Bachelor of Engineering -B.E ",
+  },
+  {
+    id: 2,
+    img: "https://asset.brandfetch.io/idtxt2_guu/idtTHffVep.jpeg?updated=1708238013797",
+    school: "Sri Chaitanya Inter college- Ramanbhavan",
+    date: "June 2014 - Apr 2016",
+    grade: "97%",
+    desc: "I completed my Intermediate education majoring on Mathematics.",
+    degree: "Math,Physics and Chemestry - MPC",
+  },
+];
+
+export const projects = [
+
+  {
+    id: 1,
+    title: "IPL CricStats",
+    description:
+      "Developed an IPL dashboard with an intuitive interface, providing comprehensive insights into all IPL teams.",
+    image:
+      "https://i.pinimg.com/564x/8e/de/36/8ede362f63fad806df4ac34df179e031.jpg",
+    tags: ["React Js", "Spring Boot", "MySQL"],
+    category: "web app",
+    //github: "https://github.com/rishavchanda/Breaking-Bad",
+    //webapp: "https://breaking-bad-webapp.netlify.app",
+  },
+  {
+    id: 2,
+    title: "ICARE HealthPulse",
+    description:
+      "Designed a hospital management system that helps the health care center register the patients and manage their records more effectively by automating and computerizing the paper-based documentation process.",
+    image:
+      "https://i.pinimg.com/736x/22/09/6a/22096a6719c60d3514c5d160bb59296c.jpg",
+    tags: ["Javascript", "Java", "SQL"], 
+    category: "android app",
+    //github: "https://github.com/rishavchanda/Quiz-Earn",
+   // webapp: "https://github.com/rishavchanda/Quiz-Earn",
+  },
+  {
+    id: 3,
+    title: "ShopEase",
+    description:
+      " Built an e-commerce web application using ReactJS as front-end, Spring Boot for server-side API development.",
+    image:
+      "https://i.pinimg.com/564x/69/35/2e/69352e36b46fcffe5a033b0881460872.jpg",
+    tags: ["Spring MVC", "Javascript"],
+    category: "machine learning",
+    //github: "https://github.com/rishavchanda/Face-Recodnition-AI-with-Python",
+    //webapp: "https://github.com/rishavchanda/Face-Recodnition-AI-with-Python",
+  },
+];
+
+// export const TimeLineData = [
+//   { year: 2017, text: "Started my journey" },
+//   { year: 2018, text: "Worked as a freelance developer" },
+//   { year: 2019, text: "Founded JavaScript Mastery" },
+//   { year: 2020, text: "Shared my projects with the world" },
+//   { year: 2021, text: "Started my own platform" },
+// ];
---
+++ src/utils/Themes.js
@@ -0,0 +1,23 @@
+export const darkTheme = {
+    bg:"#2C2C2C", // Update background color
+    bgLight: "#333333", // Update light background color
+    primary:"#FF5733", // Update primary color
+    text_primary:"#FFFFFF", // Update primary text color
+    text_secondary:"#CCCCCC", // Update secondary text color
+    card:"#1E1E1E", // Update card color
+    card_light: '#212121', // Update light card color
+    button:"#FF5733", // Update button color
+    white:"#FFFFFF", // Update white color
+    black:"#000000", // Update black color
+}
+
+
+export const lightTheme = {
+    bg:"#F9F9F9", // Update background color
+    bgLight: "#FFFFFF", // Update light background color
+    primary:"#4CAF50", // Update primary color
+    text_primary:"#333333", // Update primary text color
+    text_secondary:"#666666", // Update secondary text color
+    card:"#FFFFFF", // Update card color
+    button:"#008CBA", // Update button color
+}
